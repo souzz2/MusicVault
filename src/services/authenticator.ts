65c86b465296a30a1354a2556f776f9967c9019e
souzz2 <108484845+souzz2@users.noreply.github.com>
2024-11-04 20:43:35 -0300

    Create authenticator.ts

---
+++ src/services/authenticator.ts
@@ -0,0 +1,12 @@
+import * as jwt from "jsonwebtoken";
+import { authenticationData } from "../users/dataUsers";
+
+export const generateToken = (payload: authenticationData): string => {
+  return jwt.sign(payload, process.env.JWT_KEY as string, {
+    expiresIn: "24hrs",
+  });
+};
+
+export const getTokenData = (token: string): authenticationData => {
+  return jwt.verify(token, process.env.JWT_KEY as string) as authenticationData;
+};
