dc372966af136bcbb52dfd0917fab879968b0565
Werneck <mateuswerneckelizeu@yahoo.com.br>
2024-11-12 12:14:22 -0300

    Update dataArtists.ts

--- src/data/dataArtists.ts
+++ src/data/dataArtists.ts
@@ -1,23 +1,52 @@
 import connection from "../connection";
-import { artist } from "../types/typesArtists";
 
 export class artistData {
-  getArtistByIdData = async (id: string) => {
-    return await connection("artists")
-      .where("idartist", "=", id)
-      .orderBy("idartist", "asc")
-      .limit(10);
+  getArtistByIdData = async (id: string): Promise<any> => {
+    try {
+      const result = await connection("artists")
+        .where("idartist", "=", id)
+        .orderBy("idartist", "asc")
+        .limit(1); 
+      if (!result.length) {
+        throw new Error(`Artista com id ${id} não encontrado`);
+      }
+      return result[0];
+    } catch (error) {
+      throw new Error("Erro ao buscar artista por ID");
+    }
   };
 
-  getArtistsByNameData = async (name: string) => {
-    return await connection("artists")
-      .select("nameartist")
-      .where("nameartist", "like", `%${name}%`)
-      .orderBy("nameartist", "asc")
-      .limit(5);
+  getArtistsByNameData = async (name: string): Promise<any> => {
+    try {
+      const result = await connection("artists")
+        .select("nameartist")
+        .where("nameartist", "like", `%${name}%`)
+        .orderBy("nameartist", "asc")
+        .limit(5);
+
+      if (!result.length) {
+        throw new Error("Nenhum artista encontrado com esse nome.");
+      }
+
+      return result;
+    } catch (error) {
+      throw new Error("Erro ao buscar artistas por nome");
+    }
   };
 
-  getArtistsData = async () => {
-    return await connection("artists").orderBy("idartist", "asc").limit(10);
+  getArtistsData = async (): Promise<any> => {
+    try {
+      const result = await connection("artists")
+        .orderBy("idartist", "asc")
+        .limit(10);
+
+      if (!result.length) {
+        throw new Error("Não há artistas disponíveis.");
+      }
+
+      return result;
+    } catch (error) {
+      throw new Error("Erro ao buscar todos os artistas");
+    }
   };
 }
