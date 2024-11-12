d3070d6cfb5a165d510fc24e46883b2e5921de54
Werneck <mateuswerneckelizeu@yahoo.com.br>
2024-11-12 12:14:19 -0300

    Create artistController.ts

---
+++ src/controller/artistController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { ArtistBusiness } from "../business/ArtistBusiness";
+
+export class ArtistController {
+  artistBusiness = new ArtistBusiness();
+
+  searchArtistsByName = async (req: Request, res: Response) => {
+    try {
+      const name = req.query.name as string;
+      const artists = await this.artistBusiness.searchArtistsByName(name);
+      res.status(200).json({ artists });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message || "Erro ao buscar artista", error });
+    }
+  };
+
+  getArtistsById = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const artist = await this.artistBusiness.getArtistsMusic(id);
+      res.status(200).json({ artist });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message || "Erro ao buscar artista", error });
+    }
+  };
+
+  getAllArtists = async (req: Request, res: Response) => {
+    try {
+      const artists = await this.artistBusiness.getAllArtists();
+      res.status(200).json({ artists });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message || "Erro ao buscar artistas", error });
+    }
+  };
+}
