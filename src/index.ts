8024db57d4cde18d14c86bd153533a807698d174
Mateus <mateuswerneckelizeu@yahoo.com.br>
2024-12-01 19:45:04 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,35 +14,8 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-//post para fazer login
-
-/*
-app.get("/search", async (req, res) => {
-  const name = req.query.name?.toString().toLowerCase();
-  try {
-    if (!name) {
-      throw new Error('O parâmetro de busca "name" é obrigatório.');
-    }
-
-    const [musics, albums, artists] = await Promise.all([
-      searchMusicByName(name),
-      searchAlbumsByName(name),
-      searchArtistsByName(name),
-    ]);
-
-    if (!musics.length && !albums.length && !artists.length) {
-      throw new Error("Nenhuma música, álbum ou artista foi encontrada.");
-    }
-
-    res.status(200).json({ musics, albums, artists });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Erro ao buscar música, álbum ou artista", error });
-  }
-});*/
 
 app.use("/albums", albumRouter);
 app.use("/artists", artistRouter);
 app.use("/musics", musicRouter);
-app.use("/user", userRouter);
+app.use("/", userRouter);
